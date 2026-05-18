613e0b3fb9fc17b5bca86721d2767e747d51ab0f
Swalih Mohammed <swalih3409@gmail.com>
2026-05-18 12:45:09 +0400

    folder structure done

---
+++ template.sh
@@ -0,0 +1,15 @@
+#Creating directories for the project
+mkdir -p src
+mkdir -p research
+
+#creating files for the project
+touch src/__init__.py #constructor file for the src package
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully!" 
