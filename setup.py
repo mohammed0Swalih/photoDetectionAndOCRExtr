34741b83fce0fb5253103d5fd7604a2e05d53f09
Swalih Mohammed <swalih3409@gmail.com>
2026-05-20 10:18:02 +0400

    trials tested, main app.py and other py files created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="photoBot",
+    version="0.1.0",
+    author="Mohammed Swalih",
+    author_email="swalih3409@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
